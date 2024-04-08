ec78018ed6164b144752bbf183ed2a67384edead
Yme <yghuang@mails.ccnu.edu.cn>
2024-04-08 11:26:32 +0200

    v4.2: new Efficiency maker for PID efficiency design

--- utils/EffMaker.h
+++ utils/EffMaker.h
@@ -1,5 +1,9 @@
 /*
 
+    Version 3.6 (08.04.2024)
+
+    1. PID efficiency now has no Vz and centrality dependence, instead, now we are suing pt-y dependent PID efficiency
+
     Version 3.5 (12.01.2024)
 
     1. Now the efficiency files includes 0.5 to 0.6 rapidity bins (2 more rapidity index)
@@ -48,10 +52,13 @@ class EffMaker{
         TH2F* tof_pro[nCent][nVz];
         TH2F* tof_pbar[nCent][nVz];
 
-        TH1D* pid_pro[nVz];
-        TH1D* pid_pbar[nVz];
+        // TH1D* pid_pro[nVz];
+        // TH1D* pid_pbar[nVz];
 
-        TH1D* th1;
+        TH2F* pid_pro;
+        TH2F* pid_pbar;
+
+        // TH1D* th1;
         TH2F* th2;
 
         // version 3
@@ -75,7 +82,8 @@ class EffMaker{
         void ReadInEffFile(const char* tpc, const char* tof, const char* pid, const char* nSigTag);
         double GetTpcEff(bool positive, double pt, double y, int cent, double vz);
         double GetTofEff(bool positive, double pt, double y, int cent, double vz);
-        double GetPidEff(bool positive, double p, double vz);
+        // double GetPidEff(bool positive, double p, double vz);
+        double GetPidEff(bool positive, double pt, double y);
         // version 3 new
         int YPSplit(double y);
         int VzSplit(double vz);
