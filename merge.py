fb02c0c64564eb572eaf1b9d2455e17be6738d61
Yme <yghuang@mails.ccnu.edu.cn>
2024-07-30 23:47:13 +0200

    v7.0: New merge design

--- merge.py
+++ merge.py
@@ -3,32 +3,36 @@ import sys
 from yLog import yLog
 
 taskName = sys.argv[1]
-outDir = sys.argv[2]
-mergeDir = sys.argv[3]
+scanName = sys.argv[2]
+iterIdx = sys.argv[3]
+mergeIdx = sys.argv[4]
+fileList = sys.argv[5]
+targetDir = sys.argv[6]
 
-taskName = taskName + '.root'
-
-l = yLog(f'.{taskName}.merge.log')
+l = yLog(f'.{taskName}.iter{iterIdx}.log')
 
 l.log(f'{taskName=}')
-l.log(f'{outDir=}')
-l.log(f'{mergeDir=}')
+l.log(f'{scanName=}')
+l.log(f'{iterIdx=}')
+l.log(f'{mergeIdx=}') # just to show the merge job index
+l.log(f'{fileList=}')
+l.log(f'{targetDir=}')
 
-lists = os.listdir(outDir)
-lists = [f'{outDir}/{item}/{taskName}' for item in lists]
-
+lists = open(fileList).readlines()
+lists = [item.strip() for item in lists]
 n = len(lists)
 
 baseFile = lists.pop(0)
+saveName = f'{taskName}.{scanName}.root'
 l.log(f'Now hadd 1 / {n} : {baseFile}')
-os.system(f'cp {baseFile} {mergeDir}/tmp.{taskName}')
+os.system(f'cp {baseFile} {targetDir}/tmp.{saveName}')
 for idx, item in enumerate(lists):
     l.log(f'Now hadd {idx + 2} / {n} : {item}')
     if not os.path.exists(item):
         l.log(f'{item} does not exist, skip!')
         continue
-    os.system(f'hadd {mergeDir}/{taskName} {item} {mergeDir}/tmp.{taskName}')
-    os.system(f'mv {mergeDir}/{taskName} {mergeDir}/tmp.{taskName}')
-os.system(f'mv {mergeDir}/tmp.{taskName} {mergeDir}/{taskName}')
+    os.system(f'hadd {targetDir}/{saveName} {item} {targetDir}/tmp.{saveName}')
+    os.system(f'mv {targetDir}/{saveName} {targetDir}/tmp.{saveName}')
+os.system(f'mv {targetDir}/tmp.{saveName} {targetDir}/{saveName}')
 
 l.log('All done.')
